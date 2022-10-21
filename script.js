7b9a5efd1983f8d852d68ea7187f58fb054321b1
sardor <sultonov.sardor19@gmail.com>
2022-10-21 15:55:38 +0500

    exam-3

---
+++ script.js
@@ -0,0 +1,16 @@
+// function qalinligi(){
+//   let buttons=document.querySelector('select').value;
+//   let optionAll=document.querySelector('razmeri').value;
+
+//   if(optionAll[buttons - 1] != null){
+//     document.getElementById("noni").innerHTML = "optionAll[buttons - 1] != null .innerHTML";
+//   }
+// }
+
+function input(){
+  let ustida = document.querySelector("mahsulot");
+  let checked = document.querySelector("input").value;
+  checked.addEventListener('click').appendChild("ustida");
+}
+
+input()
